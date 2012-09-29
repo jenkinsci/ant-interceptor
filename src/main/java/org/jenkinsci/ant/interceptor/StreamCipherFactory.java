cf04c8dbb90b2e771b00a334219777a4efce865f
Kohsuke Kawaguchi <kk@kohsuke.org>
2012-09-29 13:15:57 -0700

    CFB8 mode to pass every byte to the other side.

--- src/main/java/org/jenkinsci/ant/interceptor/StreamCipherFactory.java
+++ src/main/java/org/jenkinsci/ant/interceptor/StreamCipherFactory.java
@@ -4,6 +4,7 @@ import javax.crypto.Cipher;
 import javax.crypto.CipherInputStream;
 import javax.crypto.CipherOutputStream;
 import javax.crypto.SecretKey;
+import javax.crypto.spec.IvParameterSpec;
 import java.io.InputStream;
 import java.io.OutputStream;
 import java.security.GeneralSecurityException;
@@ -20,17 +21,17 @@ public class StreamCipherFactory {
 
     public InputStream wrap(InputStream in) throws GeneralSecurityException {
         Cipher decrypt = Cipher.getInstance(ALGORITHM);
-        decrypt.init(Cipher.DECRYPT_MODE, key);
+        decrypt.init(Cipher.DECRYPT_MODE, key, new IvParameterSpec(key.getEncoded()));
 
         return new CipherInputStream(in,decrypt);
     }
 
     public OutputStream wrap(OutputStream out) throws GeneralSecurityException {
         Cipher decrypt = Cipher.getInstance(ALGORITHM);
-        decrypt.init(Cipher.ENCRYPT_MODE, key);
+        decrypt.init(Cipher.ENCRYPT_MODE, key, new IvParameterSpec(key.getEncoded()));
 
         return new CipherOutputStream(out,decrypt);
     }
 
-    private static final String ALGORITHM = "AES/CBC";
+    private static final String ALGORITHM = "AES/CFB8/NoPadding";
 }
